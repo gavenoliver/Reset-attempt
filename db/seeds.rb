d407e60d03f741853262acab2f0bf3d780bcaa9d
Jordan Hudgens <jordan@devcamp.com>
2017-06-28 17:50:53 +0000

    Integrated new and created actions for portfolios

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,7 +34,7 @@ puts "10 blog posts created"
   9.times do |portfolio_item|
     Portfolio.create!(
       title: "Portfolio title: #{portfolio_item}",
-      subtitile: "Ruby on Rails",
+      subtitle: "Ruby on Rails",
       body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
     aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
     irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
