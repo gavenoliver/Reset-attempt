9fa2f2a5e39ca4da33aa1e941b8313e1a5d7fe5e
Jordan Hudgens <jordan@devcamp.com>
2017-07-26 16:06:56 +0000

    Impletemented content tag helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def sample_helper
     "<p>My helper</p>" .html_safe
   end
+  
+  def source_helper(layout_name)
+    if session[:source]
+      greeting = "Thanks for visiting me on #{session[:source]} and you are on the #{layout_name} layout"
+     content_tag(:p, greeting, class: "source-greeting" )
+    end
+  end
 end
+
+
+
